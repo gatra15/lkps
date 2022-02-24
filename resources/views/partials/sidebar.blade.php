a638cd5b2da79076a627fc13b8ea106593efef3d
Pasyah Vegananda <pasyahwivegananda@gmail.com>
2022-02-24 16:54:00 +0700

    perubahan sedikit

--- resources/views/partials/sidebar.blade.php
+++ resources/views/partials/sidebar.blade.php
@@ -14,17 +14,24 @@
             <a class="nav-link" data-widget="pushmenu" href="#" role="button"><i class="fas fa-bars"></i></a>
           </li>       
         </ul>
-          
+
+        <ul class="navbar-nav text-right  ml-auto flex-nowrap">
+            <li class="nav-item">
+              <a class="nav-link" data-widget="pushmenu" href="#" role="button"><i class="fas fa-sign-out-alt"></i> Logout </a>
+            </li>       
+          </ul>
       </nav>
+
       <!-- /.navbar -->
     
       <!-- Main Sidebar Container -->
       <aside class="main-sidebar sidebar-dark-primary elevation-4">
         <!-- Brand Logo -->
-        <a href="index3.html" class="brand-link">
-          <img src="{{ asset('dist/img/AdminLTELogo.png') }}" alt="AdminLTE Logo" class="brand-image img-circle elevation-3" style="opacity: .8">
-          <span class="brand-text font-weight-light">AdminLTE 3</span>
-        </a>
+            <!-- Brand Logo -->
+            <a href="index3.html" class="brand-link">
+              <img src="{{ asset('dist/img/AdminLTELogo.png') }}" alt="AdminLTE Logo" class="brand-image img-circle elevation-3" style="opacity: .8">
+              <span class="brand-text font-weight-light">Sistem Informasi Akreditasi Program Studi Fakultas Teknik Universitas Diponegoro</span>
+            </a>
     
         <!-- Sidebar -->
         <div class="sidebar">
@@ -90,13 +97,13 @@
                 <ul class="nav nav-treeview ml-3">
                   <li class="nav-item">
                     <a href="pages/layout/top-nav.html" class="nav-link">
-                      <i class="nav-icon far fa-circle"></i>
+                        <i class="nav-icon fas fa-angle-double-right"></i>
                       <p>Profil Dosen</p>
                     </a>
                   </li>
                   <li class="nav-item">
                     <a href="pages/layout/top-nav-sidebar.html" class="nav-link">
-                      <i class="nav-icon far fa-circle"></i>
+                        <i class="nav-icon fas fa-angle-double-right"></i>
                       <p>Kinerja Dosen</p>
                     </a>
                   </li>
